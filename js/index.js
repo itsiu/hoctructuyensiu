38019cc9d8b7a148969babca623e509efe2a5ae6
Nguyễn Trọng Minh Hồng Phước <hongphuoc2597@gmail.com>
2020-04-11 15:32:17 +0700

    Version 1

---
+++ js/index.js
@@ -0,0 +1,74 @@
+(function(){
+
+	console.log('checkSystemRequirements');
+	console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
+
+    ZoomMtg.preLoadWasm();
+    ZoomMtg.prepareJssdk();
+    
+    var API_KEY = 'lgWWgB6AQaSwoqrric8p5A';
+
+    var API_SECRET = 'wunkKs7AfeY0cW4pKsLBrubMxsMxovkV6xHw';
+
+    testTool = window.testTool;
+    //document.getElementById('display_name').value = "CDN" + ZoomMtg.getJSSDKVersion()[0] + testTool.detectOS() + "#" + testTool.getBrowserInfo();
+
+    document.getElementById('join_meeting').addEventListener('click', function(e){
+        e.preventDefault();
+
+        if(!this.form.checkValidity()){
+            alert("Enter Name and Meeting Number");
+            return false;
+        }
+
+        
+        var meetConfig = {
+            apiKey: API_KEY,
+            apiSecret: API_SECRET,
+            meetingNumber: parseInt(document.getElementById('meeting_number').value),
+            userName: document.getElementById('display_name').value,
+            passWord: document.getElementById('meeting_pwd').value,
+            leaveUrl: "http://www.siu.edu.vn/",
+            role: parseInt(document.getElementById('meeting_role').value, 10)
+        };
+
+
+        var signature = ZoomMtg.generateSignature({
+            meetingNumber: meetConfig.meetingNumber,
+            apiKey: meetConfig.apiKey,
+            apiSecret: meetConfig.apiSecret,
+            role: meetConfig.role,
+            success: function(res){
+                console.log(res.result);
+            }
+        });
+
+        ZoomMtg.init({
+            leaveUrl: 'http://www.siu.edu.vn/',
+            isSupportAV: true,
+            success: function () {
+                ZoomMtg.join(
+                    {
+                        meetingNumber: meetConfig.meetingNumber,
+                        userName: meetConfig.userName,
+                        signature: signature,
+                        apiKey: meetConfig.apiKey,
+                        passWord: meetConfig.passWord,
+                        success: function(res){
+                            $('#nav-tool').hide();
+                            console.log('join meeting success');
+                        },
+                        error: function(res) {
+                            console.log(res);
+                        }
+                    }
+                );
+            },
+            error: function(res) {
+                console.log(res);
+            }
+        });
+
+    });
+
+})();
